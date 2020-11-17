8a0e234684ebaaf4e75610497da3cdae8ee28f76
RoTour <robin.tourne@ynov.com>
2020-11-17 15:45:08 +0100

    Add edit Contact in db

--- Contact/app/Http/Controllers/ContactController.php
+++ Contact/app/Http/Controllers/ContactController.php
@@ -9,14 +9,15 @@ use Illuminate\Support\Facades\Http;
 
 class ContactController extends Controller
 {
+
     /**
      * Display a listing of the resource.
      */
     public function index()
     {
         return view("contacts/index", ["contacts" => DB::select("select * from contacts")]);
-
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -26,16 +27,13 @@ class ContactController extends Controller
         return view("contacts/create");
     }
 
+
     /**
      * Store a newly created resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
      */
     public function store(Request $request)
     {
-        $query = "INSERT INTO
-                    contacts (firstname, lastname, email, phone)
-                    values (" .
+        $query = "INSERT INTO contacts (firstname, lastname, email, phone) values (" .
             "'" . $request->input('firstname') . "'" . ", " .
             "'" . $request->input('lastname') . "'" . ", " .
             "'" . $request->input('email') . "'" . ", " .
@@ -44,48 +42,46 @@ class ContactController extends Controller
         return redirect()->action([ContactController::class, 'index']);
     }
 
+
     /**
      * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
-        return "in show method";
+        return "Sowing: " . DB::select("select * from contacts where id=" . $id);
     }
+
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * //     * @return \Illuminate\Http\Response
      */
     public function edit(int $id)
     {
-        return view("contacts/index", ["contacts" => DB::select("select * from contacts")]);
+        return view(
+            "contacts/edit",
+            ["contact" => DB::select("select * from contacts where id=" . $id)]);
     }
+
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * //     * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
-        return "UPDATE ROUTE";
-//        $except = $request->except("_token");
-//        self::$storage[$id] = $except;
-//        return view("contacts/index", ["contacts" => self::$storage]);
+        $query =
+            "UPDATE contacts SET " .
+            "firstname = " . "'" . $request->input("firstname") . "'" . ", " .
+            "lastname = " . "'" . $request->input("lastname") . "'" . ", " .
+            "email = " . "'" . $request->input("email") . "'" . ", " .
+            "phone = " . "'" . $request->input("phone") . "'" .
+            "WHERE id=" . $id;
+        DB::insert($query);
+        return redirect()->action([ContactController::class, 'index']);
     }
+
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
